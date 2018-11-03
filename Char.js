dbaf29fb497e4e9da3dadbf804948e157c0f3ce3
sigursteinn0 <32844951+sigursteinn0@users.noreply.github.com>
2018-11-03 15:54:19 +0000

    Update Char.js

--- Char.js
+++ Char.js
@@ -93,8 +93,9 @@ Char.prototype.computeGravity = function () {
 
 var NOMINAL_RIGHT = +6;
 var NOMINAL_LEFT  = -6;
-var NOMINAL_JUMP  = -0.5;
-var NOMINAL_GRAVITY = +0.2;
+var NOMINAL_IJUMP = -6;
+var NOMINAL_JUMP  = -2;
+var NOMINAL_GRAVITY = +2;
 
 var JUMP_INIT = true;
 var JUMP_TIMER = 0;
@@ -108,8 +109,8 @@ Char.prototype.movement = function (du) {
     }
     if (keys[this.KEY_JUMP]) {
         if (JUMP_INIT && JUMP_TIMER == 0) {
-            JUMP_TIMER = 40;
-            this.velY = +NOMINAL_JUMP * du;
+            JUMP_TIMER = 20;
+            this.velY = +NOMINAL_IJUMP * du;
         } else if (JUMP_INIT) {
             this.velY += NOMINAL_JUMP * du;
         } 
