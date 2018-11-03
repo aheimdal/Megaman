b9bf8a44d1edd725ffcbe5f665ac038ca1dffd37
tomastryggva <tomastryggva@hotmail.com>
2018-11-03 16:01:01 +0000

    Stórar breytingar!

--- background.js
+++ background.js
@@ -26,11 +26,12 @@ function canvasSpaceGame() {
       //stars(); 
 
       document.body.style.backgroundColor = "#f3f3f3";
-      document.body.style.backgroundImage = "url('Mega.png')";
+      document.body.style.backgroundImage = "url('images/Mega.png')";   
+
 
       var img=new Image();
       img.onload=start;
-      img.src="kell.jpg";
+      img.src="images/bak2.png";
       function start(){
           ctx.drawImage(img,0,0);
       }
