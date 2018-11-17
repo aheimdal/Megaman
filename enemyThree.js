03f6a732aeba4b86104bff76752df58ea3f2b21e
sigursteinn0 <32844951+sigursteinn0@users.noreply.github.com>
2018-11-17 19:46:01 +0000

    s

--- enemyThree.js
+++ enemyThree.js
@@ -46,6 +46,9 @@ enemyThree.prototype.update = function (du) {
         this.health--;
     }
 
+    if (entityManager._char[0].cx < this.cx) this.goblinFacing = 1;
+    else this.goblinFacing = 0;
+
     this.movement(du);
 
     this.maybeShoot();
