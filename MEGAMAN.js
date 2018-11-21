1855e73a16efa7e82a12c835c8b319d7074446bb
sigursteinn0 <32844951+sigursteinn0@users.noreply.github.com>
2018-11-21 17:12:24 +0000

    damn

--- MEGAMAN.js
+++ MEGAMAN.js
@@ -88,8 +88,6 @@ var g_useGravity = false;
 var g_useAveVel = true;
 var g_renderSpatialDebug = false;
 
-var KEY_MIXED   = keyCode('M');;
-var KEY_GRAVITY = keyCode('G');
 var KEY_AVE_VEL = keyCode('V');
 var KEY_SPATIAL = keyCode('X');
 
@@ -104,11 +102,6 @@ var KEY_2 = keyCode('2');
 var KEY_K = keyCode('K');
 
 function processDiagnostics() {
-
-    if (eatKey(KEY_MIXED))
-        g_allowMixedActions = !g_allowMixedActions;
-
-    if (eatKey(KEY_GRAVITY)) g_useGravity = !g_useGravity;
 
     if (eatKey(KEY_AVE_VEL)) g_useAveVel = !g_useAveVel;
 
@@ -319,10 +312,12 @@ function preloadDone() {
 
     createInitialChar();
 
+    /*
     AudioBank.songOne.loop = true;
     AudioBank.songTwo.loop = true;
     AudioBank.songThree.loop = true;
     AudioBank.songFour.loop = true;
+    */
 
     main.init();
 
