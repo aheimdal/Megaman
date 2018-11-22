b12e83ee0453240afc6d4f45c185f730d8a3b41d
steini96 <steini.tki@gmail.com>
2018-11-22 23:51:14 +0000

    commentation

--- Platform.js
+++ Platform.js
@@ -33,97 +33,63 @@ Platform.prototype.scale = 0;
 
 
 Platform.prototype.update = function (du) {
-
     spatialManager.unregister(this);
-
-
     if(this._isDeadNow){
         return entityManager.KILL_ME_NOW;
     }
-
     spatialManager.register(this);
 
 };
 
-
+// function to get radius
 Platform.prototype.getRadius = function () {
     return this.radius;
 };
 
+// function for calculation of movement
 Platform.prototype.calculateMovement = function (entity) {
+    // set variables to work with
     var higherBound = this.cy - this.radius;
     var lowerBound = this.cy + this.radius;
     var leftBound = this.cx - this.radius;
     var rightBound = this.cx + this.radius;
-    var radius = 45;//entity.getRadius();
+    var radius = 45;
 
-    //console.log(this.radius);
-    //console.log(radius);
-
-    /*if (leftBound <= entity.cx + radius ||
-        (rightBound >= entity.cx - radius)) {
-        entity.stopX();// setur velocity í 0
-    }*/
-    //console.log("hit");
-    if(entity.cx<leftBound-28){
+    ///// Here the collision is resolved/////
+    if(entity.cx<leftBound-28){// pushes the entity to the left of the platform
+      // if it came from the left
       entity.stopX();
-      entity.cx = leftBound-radius;//leftBound - radius - 1;
-    }else if(entity.cx>rightBound+28){
+      entity.cx = leftBound-radius;
+    }else if(entity.cx>rightBound+28){// pushes the entity to the right of the
+      // platform if it came from the left
       entity.stopX();
       entity.cx = rightBound + radius;
-    }else if (entity.isFalling()) {
-        //if (higherBound > entity.cy - radius) {
+    }else if (entity.isFalling()) {// pushes the entity up one pixel over the
+      // platform if the entity is jumping
       entity.ground()
       entity.cy = higherBound - radius-1;
-        //}
-    }else if(entity.isJumping()&&(entity.cy<502)){
-        //if (lowerBound < entity.cy + radius) {
+    }else if(entity.isJumping()&&(entity.cy<502)){// pushes the entity just
+      // below the platform if it is jumping and is not on the ground
       entity.fall();
       entity.cy = lowerBound + radius;
-        //}
-    }else if(entity.cx<this.cx){
+    }else if(entity.cx<this.cx){// if the entity is on the ground and is to the
+      // left of the center of the platform it is pushed to
+      // the left of the platform
       entity.stopX();
-      entity.cx = leftBound-radius+8;//leftBound - radius - 1;
-    }else if(entity.cx>this.cx){
+      entity.cx = leftBound-radius+8;
+    }else if(entity.cx>this.cx){// if the entity is on the ground and is to the
+      // right of the center of the platform it is pushed to the
+      // right of the platform
       entity.stopX();
       entity.cx = rightBound + radius-8;
     }
 };
 
-
-/*var kubbur = new Image();
-kubbur.src = '"./images/kubbur.jpg"';
-*/Platform.prototype.render = function (ctx) {
-  /*  ctx.fillRect(this.cx - this.radius,
-                this.cy - this.radius,
-                this.radius*2,
-                this.radius*2);
-*/
-/*var kubbur = new Image();
-kubbur.src = '"./images/kubbur.jpg"';
-ctx.drawImage(kubbur, this.cx - this.radius,
-              this.cy - this.radius,
-              this.radius*2,
-              this.radius*2);*/
-
-              var origScale = this.sprite.scale;
-              // pass my scale into the sprite, for drawing
-              this.sprite.scale = this.scale;
-              //console.log(this.sprite.width);
-
-              //console.log(this.sprite.width);
-              this.sprite.drawCentredAt(
+// function that renders platforms
+Platform.prototype.render = function (ctx) {
+              this.sprite.scale = this.scale;// set the scale of the sprite
+              this.sprite.drawCentredAt(// draw the platform
                   ctx, this.cx, this.cy, this.rotation
               );
-
-/*ctx.fillStyle="red";
-ctx.fillRect(this.cx - this.radius,
-            this.cy - this.radius,
-            this.radius*2,
-            this.radius*2);*/
-/*ctx.fillStyle="black";
-ctx.beginPath();
-ctx.arc(this.cx,this.cy,25,0,2*Math.PI);
-ctx.stroke();*/
 
 };
