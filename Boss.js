55f4416d9a5866b339c8149ab19fd2dcfc4ced43
sigursteinn0 <32844951+sigursteinn0@users.noreply.github.com>
2018-11-23 15:40:38 +0000

    Update Boss.js

--- Boss.js
+++ Boss.js
@@ -232,6 +232,12 @@ Boss.prototype.deathHandler = function () {
     if (this._isDeadNow) {
       this._isDeadNow = false;
       this.health--;
+      if (util.randRange(0,10) < 1) {
+        entityManager.generateHealthPickup({
+          cx:this.cx,
+          cy:this.cy-5
+        });
+      }    
       if (this.health === 0) this.deathTimer = 100;
     }
   }
